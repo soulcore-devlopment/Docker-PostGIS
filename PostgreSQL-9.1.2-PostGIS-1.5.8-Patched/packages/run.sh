519e56847bac1cb460227a215052f686e2095ba2
guilherme.prado@soulcore.com.br <guilherme.prado@soulcore.com.br>
2019-09-06 20:48:54 -0300

    Working with postgis deployed

--- PostgreSQL-9.1.2-PostGIS-1.5.8-Patched/packages/run.sh
+++ PostgreSQL-9.1.2-PostGIS-1.5.8-Patched/packages/run.sh
@@ -6,25 +6,8 @@ chown postgres:postgres ${POSTGRES_DATA_FOLDER}
 chmod 700 ${POSTGRES_DATA_FOLDER}
 echo "postgres:${POSTGRES_PASSWD}" | chpasswd -e
 
-function create_postgis()
-{
-    # plpgsql already exists
-    # su postgres -c "createdb ${POSTGIS_DATABASE} && createlang plpgsql ${POSTGIS_DATABASE}" \
-    su postgres -c "createdb ${POSTGIS_DATABASE}" \
-    && su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/postgis.sql" \
-    && su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/spatial_ref_sys.sql"
-    # && su postgres -c "psql -h localhost -U postgres -p 5432 -tc \"UPDATE pg_database SET datistemplate = false, datconnlimit=-1, datallowconn=true WHERE datname = '${POSTGIS_DATABASE}';\""
-    # not working
-    # && su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/postgis_comments.sql"
-}
-
-function check_postgis()
-{
-    su postgres -c "psql -h localhost -U postgres -p 5432 -tc \"SELECT 1 FROM pg_database WHERE datname = '${POSTGIS_DATABASE}';\"" | grep -q 1 || create_postgis
-}
-
-function setup_database()
-{
+# Check if data folder is empty. If it is, start the dataserver
+if ! ["$(ls -A ${POSTGRES_DATA_FOLDER})" ]; then
     su postgres -c "initdb  --encoding=${ENCODING} --locale=${LOCALE}.${ENCODING} --lc-collate=${LOCALE}.${ENCODING}  --lc-monetary=${LOCALE}.${ENCODING}  --lc-numeric=${LOCALE}.${ENCODING}  --lc-time=${LOCALE}.${ENCODING}  -D ${POSTGRES_DATA_FOLDER}"
 
     # Modify basic configutarion
@@ -33,14 +16,16 @@ function setup_database()
 
     # Establish postgres user password and run the database
     su postgres -c "pg_ctl -w -D ${POSTGRES_DATA_FOLDER} start" \
-    && su postgres -c "psql -h localhost -U postgres -p 5432 -c \"alter role postgres password '${POSTGRES_PASSWD}';\"" \
+    && su postgres -c "createdb ${POSTGIS_DATABASE}" \
+    && su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/postgis.sql" \
+    && su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/spatial_ref_sys.sql" \
+    && su postgres -c "psql -h localhost -U postgres -p 5432 -tc \"alter role postgres password '${POSTGRES_PASSWD}';\"" \
     && su postgres -c "pg_ctl -w -D ${POSTGRES_DATA_FOLDER} stop"
-}
-
-# Check if data folder is empty. If it is, start the dataserver
-if ! ["$(ls -A ${POSTGRES_DATA_FOLDER})" ]; then
-    setup_database
 fi
 
 # Start the database
-su postgres -c "postgres -D $POSTGRES_DATA_FOLDER" && check_postgis
+su postgres -c "postgres -D $POSTGRES_DATA_FOLDER"
+
+#su postgres -c "psql -h localhost -U postgres -p 5432 -tc \"SELECT 1 FROM pg_database WHERE datname = '${POSTGIS_DATABASE}';\"" | grep -q 1 || su postgres -c "createdb ${POSTGIS_DATABASE}" \
+#&& su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/postgis.sql" \
+#&& su postgres -c "psql -h localhost -U postgres -p 5432 -d ${POSTGIS_DATABASE} -f ${POSTGIS_CONTRIB}/spatial_ref_sys.sql"
